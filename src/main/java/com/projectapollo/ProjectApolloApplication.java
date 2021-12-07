64ac3036a66802f75bbf36190732c86cb764d85a
Jonathan <Jonathananders6@gmail.com>
2021-12-07 11:21:03 -0500

    Initial commit

---
+++ src/main/java/com/projectapollo/ProjectApolloApplication.java
@@ -0,0 +1,13 @@
+package com.projectapollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApolloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApolloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectapollo/ProjectApolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectapollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApolloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
