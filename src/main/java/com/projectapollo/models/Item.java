6404ee8e07f0513863829338f236e6a26f32672f
Jonathan <Jonathananders6@gmail.com>
2021-12-08 12:43:47 -0500

    build

--- src/main/java/com/projectapollo/models/Item.java
+++ src/main/java/com/projectapollo/models/Item.java
@@ -14,7 +14,7 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String itemName;
+    private String item_name;
     private String brand;
     private Double price;
     private String category;
@@ -31,12 +31,12 @@ public class Item {
         this.id = id;
     }
 
-    public String getItemName() {
-        return itemName;
+    public String getItem_name() {
+        return item_name;
     }
 
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
+    public void setItemName(String item_name) {
+        this.item_name = item_name;
     }
 
     public String getBrand() {
